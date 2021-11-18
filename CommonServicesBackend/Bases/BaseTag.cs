d8d05ba7240663f7ca903c14cbfb1eedd328ac16
mike tranchida <Mtranchi@hotmail.com>
2021-11-17 23:31:55 -0500

    Added base entities

---
+++ CommonServicesBackend/Bases/BaseTag.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CommonServicesBackend.Bases
+{
+    public abstract class BaseTag : BaseAddedByEntity
+    {
+        [MaxLength(1024)]
+        public string Description { get; set; }
+
+        [Required, StringLength(64)]
+        public string Name { get; set; }
+
+        [Required, StringLength(64)]
+        public string UrlSlug { get; set; }
+    }
+}
